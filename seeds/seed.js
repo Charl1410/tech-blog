9d4763db0976a3a7ce6d3a93d56ea1dbd01ee121
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-25 20:25:04 +0000

    added seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 const seedComments = require('./comment-seeds');
 const seedPosts = require('./post-seeds');
 const seedUsers = require('./user-seeds');
