71b17c6bb691fcc7563a5df06a214bba3a713d26
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-25 14:17:57 +0000

    added content in db/schema.sql

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//route to create a new comment if logged in 
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newComment = await Comments.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newComment);
+      // console.info(newPost)
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+module.exports = router;
